eb24098a8dd666cf2cc74ddad8031acc17151d2d
fancyaction <labcoatdisaster@gmail.com>
2021-04-13 15:55:52 -0500

    feature: add errorHandler middleware

---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+const errorHandler = (_req: Request, res: Response) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message,
+    });
+};
+
+export default errorHandler;
