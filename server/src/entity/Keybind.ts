be4d733dbefb54a1d33173654f7b89ac599077ca
Ken Cruz <ken@kencruz.ca>
2021-04-14 00:49:01 -0400

    implemented typeorm entity definitions related to #43

---
+++ server/src/entity/Keybind.ts
@@ -0,0 +1,36 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Cheatsheet } from "./Cheatsheet";
+import { CheatsheetCategory } from "./CheatsheetCategory";
+
+@Entity()
+export class Keybind {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cheatsheetId: number;
+
+  @Column()
+  categoryId: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  keyCombination: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  likes: number;
+
+  @ManyToOne((type) => Cheatsheet, (cheatsheet) => cheatsheet.keybinds)
+  cheatsheet: Cheatsheet;
+
+  @ManyToOne(
+    (type) => CheatsheetCategory,
+    (cheatsheetCategory) => cheatsheetCategory.keybinds
+  )
+  cheetsheetCategory: CheatsheetCategory;
+}
