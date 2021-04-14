e412c2b0fe0a1b02ff27e8e061342c6d71f7bb69
Seth Anderson <sethdanderson17@gmail.com>
2021-04-14 15:50:12 -0700

    Remove more boilerplate

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,3 @@
-import Head from 'next/head';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-
-export default function Home({ allPostsData }) {
-  return (
-    <div>
-      List of Cheatsheets
-    </div>
-  );
+export default function Home() {
+  return <div>List of Cheatsheets</div>;
 }
