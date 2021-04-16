ffc55d67a27cdba6b137a0ad24bc4bef2e73442a
aliselmes <a.selmes@gmail.com>
2021-04-15 20:42:05 -0500

    adds responsiveness to Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Navbar({ fixed }) {
+export default function Navbar() {
 
     const [navbarOpen, setNavbarOpen] = React.useState(false);
     const [modalOpen, setModalOpen] = React.useState(false);
@@ -10,12 +10,14 @@ export default function Navbar({ fixed }) {
             <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3 border-b border-gray">
                 <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                        <a className="text-base font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-700"  href="/">
+                        <a className="text-lg font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-700 hover:no-underline"  href="/">
                             KEYBOUND
                         </a>
                         <button className="text-gray-700 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button" onClick={() => setNavbarOpen(!navbarOpen)}>
-                            <i className="fas fa-bars"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
                         </button>
                     </div>
                     <div className={
@@ -26,19 +28,19 @@ export default function Navbar({ fixed }) {
                     >
                         <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                             <li className="nav-item">
-                                <button className="text-sm px-3 py-2 flex items-center text-xs leading-snug text-gray-700 hover:opacity-75"
+                                <button className="text-sm px-3 py-2 flex items-center text-xs leading-snug text-gray-700 hover:opacity-75 focus:outline-none"
                                 >
                                     Sign Up
                                 </button>
                             </li>
                             <li className="nav-item">
-                                <button className="text-sm px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700 hover:opacity-75"
+                                <button className="text-base px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-700 hover:opacity-75 focus:outline-none"
                                 >
-                                    Log In
-                                </button>
+                                    Login
+                                </button> 
                             </li>
                         </ul>
-                    </div>
+                    </div> 
                 </div>
             </nav>
         </>
