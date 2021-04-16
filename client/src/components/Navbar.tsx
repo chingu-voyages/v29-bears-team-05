15919d37a54dac7349af3cfba139dfae09ceec87
aliselmes <a.selmes@gmail.com>
2021-04-15 19:45:44 -0500

    creates Navbar component file

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Navbar({ fixed }) {
+
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+    const [modalOpen, setModalOpen] = React.useState(false);
+
+    return (
+        <>
+            <nav>
+                
+            </nav>
+        </>
+    );
+}
+
+
+
