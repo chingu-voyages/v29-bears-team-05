5f05cd1e4429fb745205e2ac78e99a05d66c58c5
Seth Anderson <sethdanderson17@gmail.com>
2021-04-20 15:57:11 -0700

    Add list component to sheet page

--- client/src/pages/sheets/[sheetName].js
+++ client/src/pages/sheets/[sheetName].js
@@ -1,11 +1,19 @@
 import { useRouter } from 'next/router';
+import KeybindList from '../../components/KeybindList';
+import data from '../../lib/mockData/index'
 
 const Sheet = () => {
     const router = useRouter();
     const { sheetName } = router.query;
 
+    const sheetData = data?.find(list => list[0].cheatsheet === sheetName)
+
     if (sheetName) {
-        return <div>cheatsheet: {sheetName} </div>;
+        return (
+            <div>
+                <KeybindList data={sheetData} master={true} />
+            </div>
+        );
     } else {
         return null;
     }
