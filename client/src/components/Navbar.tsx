3ca3f8741297c27d24ac748cc991a107fee6da04
aliselmes <a.selmes@gmail.com>
2021-04-21 20:35:15 -0500

    fixes indentation

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -110,7 +110,7 @@ export default function Navbar() {
                                     </Form>
                                 </Formik>
                                 <Formik
-                                    initialValues={{ email: '', username: "", password: ''}}
+                                    initialValues={{ email: '', username: '', password: ''}}
                                     validationSchema={Yup.object({
                                         email: Yup.string()
                                             .email('Invalid email address')
@@ -165,13 +165,13 @@ export default function Navbar() {
                             </div>
                             {/* Footer */}
                             <div className="flex items-center p-1 border-t border-solid border-blueGray-200 rounded-b">
-                            <button
-                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button"
-                                onClick={() => setShowModal(false)}
-                            >
-                                Close
-                            </button>
+                                <button
+                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    Close
+                                </button>
                             </div>
                         </div>
                     </div>
