bf3ade1f93352e436e669a86c9b54f456cd8a5c0
Ken Cruz <ken@kencruz.ca>
2021-04-22 18:29:15 -0400

    implemented vscode data population to database

---
+++ server/src/mockdata.ts
@@ -0,0 +1,75 @@
+import data from '../../client/src/lib/mockData/vscodeMaster.json';
+import { createConnection, getConnection } from 'typeorm';
+import { CheatsheetCategory } from './entity/CheatsheetCategory';
+import { Cheatsheet } from './entity/Cheatsheet';
+import { Keybind } from './entity/Keybind';
+
+type CategoryStore = { [key: string]: CheatsheetCategory };
+
+const init = async () => {
+  console.log('Populating data...');
+  // create vscode cheatsheet
+  let vscode = new Cheatsheet();
+  vscode.name = 'vscode';
+  vscode.logoUrl = '';
+  vscode.keybinds = [];
+  // build categories
+  let categories: CategoryStore = {};
+  data
+    .map((x) => x.cheatsheetCategory)
+    .filter((x, i, a) => a.indexOf(x) === i)
+    .forEach((el, i) => {
+      let category = new CheatsheetCategory();
+      category.name = el;
+      category.index = i;
+      category.keybinds = [];
+      categories[el] = category;
+    });
+  // build keybinds
+  let keybinds = data.map((x) => {
+    let keybind = new Keybind();
+    keybind.name = x.name;
+    keybind.keyCombination = x.keyCombination;
+    keybind.description = x.description;
+    keybind.likes = 0;
+    //add relations keybind-cheatsheet, keybind-category
+    keybind.cheatsheet = vscode;
+    keybind.cheatsheetCategory = categories[x.cheatsheetCategory];
+    // add relation category-keybind
+    categories[x.cheatsheetCategory].keybinds.push(keybind);
+    return keybind;
+  });
+  // add relation cheatsheet-keybinds
+  vscode.keybinds = keybinds;
+
+  await createConnection()
+    .then(async (conn) => {
+      // wipe out any existing data, starting from tables with foreign keys
+      await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(Keybind)
+        .execute();
+      await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(CheatsheetCategory)
+        .execute();
+      await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(Cheatsheet)
+        .execute();
+      // save to database
+      await conn.manager.save(vscode);
+      // make categories to an array for connection manager to save easily
+      const categoriesOutput = Object.values(categories);
+      await conn.manager.save(categoriesOutput);
+      await conn.manager.save(keybinds);
+    })
+    .catch((e) => console.log(e));
+
+  console.log('Data population complete!');
+};
+
+init();
