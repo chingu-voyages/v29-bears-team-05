21bba2864da1ce1f87fa8898451fad55f0890b53
Seth Anderson <sethdanderson17@gmail.com>
2021-04-21 20:51:47 -0700

    Refactor

--- client/src/components/KeybindList.tsx
+++ client/src/components/KeybindList.tsx
@@ -1,5 +1,77 @@
+import React from 'react';
 import _ from 'lodash';
-import Table from './Table/Table';
+import TextField from './Textfield';
+
+const HeartButton = () => (
+  <td className="text-sm sm:text-base p-2">
+    <button>🤍</button>
+  </td>
+);
+
+const Row = ({ record, children }) => {
+  return (
+    <tr className="text-left py-4">
+      {React.Children.map(children, (child) =>
+        React.cloneElement(child, [record])
+      )}
+    </tr>
+  );
+};
+
+const TableHeaders = ({ headers = [] }) => {
+  return (
+    <thead>
+      <tr className="text-left">
+        {headers.map((header) => (
+          <th key={header} className="text-sm sm:text-base p-2">
+            {header}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
+
+const TableRows = ({ tableData }) => {
+  return (
+    <tbody>
+      {tableData.map((record) => {
+        return (
+          <Row key={record.id} record={record}>
+            <TextField record={record} source="keyCombination" />
+            <TextField record={record} source="description" />
+            <TextField record={record} source="likes" />
+            <HeartButton />
+          </Row>
+        );
+      })}
+    </tbody>
+  );
+};
+
+const Table = ({ category, tableData, headers }) => {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="w-full max-w-2xl mx-3">
+          <h1 className="mt-11 mb-3 text-2xl p-2">{category}</h1>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <table className="table-fixed w-full max-w-2xl mx-3">
+          <colgroup>
+            <col className="w-7/12"></col>
+            <col className="w-7/12"></col>
+            <col className="w-2/12"></col>
+            <col className="w-2/12"></col>
+          </colgroup>
+          <TableHeaders headers={headers} />
+          <TableRows tableData={tableData} />
+        </table>
+      </div>
+    </div>
+  );
+};
 
 const KeybindList = ({ sheetData = [], headers, categoryField }) => {
   const categoryNames = [...new Set(sheetData.map((el) => el[categoryField]))];
--- client/src/components/Textfield.tsx
+++ client/src/components/Textfield.tsx
@@ -1,5 +1,4 @@
 const TextField = ({ record, source }) => {
-
   return (
     <td className="text-sm sm:text-base p-2">
       <span>{record[source]}</span>
