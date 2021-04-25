cab73e862ec986b881538b825cdb91499ba96976
Ken Cruz <ken@kencruz.ca>
2021-04-25 16:23:34 -0400

    implemented adding keybind to user favorites

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { validate } from 'class-validator';
 import { User } from '../entity/User';
+import { Keybind } from '../entity/Keybind';
 
 const getFavorites = async (_req: Request, res: Response) => {
   const userCredentials = { user: _req.body.user, token: _req.body.token };
@@ -24,6 +25,39 @@ const getFavorites = async (_req: Request, res: Response) => {
     res.status(404).send('User not found');
   }
 };
+
+const addFavorite = async (_req: Request, res: Response) => {
+  const userId = _req.body.user.id;
+  const queryId = _req.body.keybind.id;
+
+  const userRepository = getRepository(User);
+  const keybindRepository = getRepository(Keybind);
+
+  // get list of favorites
+  const user = (await userRepository
+    .findOneOrFail(userId, {
+      select: ['id', 'username'],
+      relations: ['userFavorites'],
+    })
+    .catch(() => res.status(404).send('User not found'))) as User;
+
+  // reject if keybind is already in list
+  const keybinds_id = user.userFavorites.map((x) => x.id);
+  if (keybinds_id.includes(queryId)) {
+    res.status(403).send('Keybind already in favorites');
+    return;
+  }
+
+  const keybind = (await keybindRepository
+    .findOneOrFail(queryId)
+    .catch(() => res.status(404).send('Keybind not found'))) as Keybind;
+
+  user.userFavorites.push(keybind);
+  await userRepository.save(user);
+  res.status(200).send(user);
+};
+
+const deleteFavorite = async (_req: Request, _res: Response) => {};
 
 const getList = async (_req: Request, res: Response) => {
   const userRepository = getRepository(User);
@@ -128,6 +162,8 @@ const deleteUser = async (req: Request, res: Response) => {
 
 export default {
   getFavorites,
+  addFavorite,
+  deleteFavorite,
   getList,
   getOneById,
   createUser,
