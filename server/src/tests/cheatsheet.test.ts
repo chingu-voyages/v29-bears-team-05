8622e42d96e632a14e7e74a21eedcdccfd211bf6
Ken Cruz <ken@kencruz.ca>
2021-05-01 08:49:16 -0400

    implemented api GET /sheet/:id test

--- server/src/tests/cheatsheet.test.ts
+++ server/src/tests/cheatsheet.test.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import supertest from 'supertest';
 import routes from '../routes';
 import { createConnection, getConnection, getRepository } from 'typeorm';
 import { Cheatsheet } from '../entity/Cheatsheet';
-import supertest from 'supertest';
+import { CheatsheetCategory } from '../entity/CheatsheetCategory';
+import { Keybind } from '../entity/Keybind';
 
 const app = express();
 app.use(express.json());
@@ -53,5 +55,36 @@ test('GET /sheet', async () => {
       expect(response.body[1].logoUrl).toEqual(
         'assets/images/photoshop-logo.png'
       );
+    });
+});
+
+test('GET /sheet/:id', async () => {
+  const cheatsheet = new Cheatsheet();
+  cheatsheet.name = 'vscode';
+
+  const keybind = new Keybind();
+  keybind.name = 'Alt + P';
+  keybind.keyCombination = 'Alt + P';
+  keybind.description = 'Show Something';
+  keybind.likes = 0;
+
+  const category = new CheatsheetCategory();
+  category.name = 'Basic';
+  category.keybinds = [keybind];
+  category.index = 0;
+
+  cheatsheet.keybinds = [keybind];
+
+  await getConnection().manager.save(keybind);
+  await getConnection().manager.save(category);
+  await getConnection().manager.save(cheatsheet);
+
+  await supertest(app)
+    .get('/sheet/1')
+    .expect(200)
+    .then((response) => {
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toEqual(1);
+      expect(response.body[0].name).toEqual('Alt + P');
     });
 });
