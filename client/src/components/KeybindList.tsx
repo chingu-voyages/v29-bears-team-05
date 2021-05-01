2b6a130cce7e3a890f3b228cb79d7076127d8b44
fancyaction <labcoatdisaster@gmail.com>
2021-04-30 19:42:58 -0500

    fix: iterator of iterator warning, add props

--- client/src/components/KeybindList.tsx
+++ client/src/components/KeybindList.tsx
@@ -1,16 +1,29 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 import _ from 'lodash';
 import Table from '../ui/Table';
 
-const KeybindList = ({ sheetData = [], columns, titleField }) => {
-  const categories = [...new Set(sheetData.map((el) => el[titleField]))];
+type Props = {
+  sheetData: any[];
+  columns: any;
+  titleField: string;
+};
 
-  return categories.map((title, i) => {
-    const tableData = _.filter(sheetData, { categoryId: i + 1 });
-    return (
-      <Table key={title} title={title} data={tableData} columns={columns} />
-    );
-  });
+const KeybindList = ({ sheetData, columns, titleField }: Props) => {
+  const categories = useMemo(
+    () => Array.from(new Set(sheetData.map((el) => el[titleField]))),
+    [sheetData, titleField]
+  );
+
+  return (
+    <React.Fragment>
+      {categories.map((title, i) => {
+        const tableData = _.filter(sheetData, { categoryId: i + 1 });
+        return (
+          <Table key={i} title={title} data={tableData} columns={columns} />
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default KeybindList;
