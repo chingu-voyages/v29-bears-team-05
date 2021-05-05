a718dff84c0c37c85eac801c0b3a540bd5fe0ec3
Ken Cruz <ken@kencruz.ca>
2021-05-04 20:46:07 -0400

    fix: move user id route to /user/id/:id

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -5,12 +5,12 @@ import authenticateToken from '../middleware/authenticateToken';
 const router = Router();
 
 router.get('/', UserController.getList);
-router.get('/:id', UserController.getOneById);
+router.get('/id/:id', UserController.getOneById);
 router.get('/favorites', authenticateToken, UserController.getFavorites);
 router.post('/favorites', authenticateToken, UserController.addFavorite);
 router.delete('/favorites', authenticateToken, UserController.deleteFavorite);
 router.post('/', UserController.createUser);
-router.patch('/:id', UserController.updateUser);
-router.delete('/:id', UserController.deleteUser);
+router.patch('/id/:id', UserController.updateUser);
+router.delete('/id/:id', UserController.deleteUser);
 
 export default router;
