b87ad833b23685143fc2032e20bb4d02eedeff74
Seth Anderson <sethdanderson17@gmail.com>
2021-05-06 14:52:13 -0700

    Start add fav to array WIP

--- client/src/pages/sheets/[sheetName].tsx
+++ client/src/pages/sheets/[sheetName].tsx
@@ -3,12 +3,41 @@ import { useQuery, UseQueryResult, useQueryClient } from 'react-query';
 import { getSheet } from '../../service/queryFns';
 import KeybindList from '../../components/KeybindList';
 import TextField from '../../components/Textfield';
+import { useState } from 'react';
+import { FavsProvider, useFavs } from '../../context/FavContext';
 
-const FavoriteButton = () => (
-  <td className="p-2 text-sm sm:text-base">
-    <button>🤍</button>
-  </td>
-);
+const FavoriteButton = ({ record }) => {
+  const context = useFavs();
+  const { favs, setFavs } = context;
+  const initialState = false;
+  // const initialState = favs.includes(record.id);
+  const [active, setActive] = useState(initialState);
+  const handleClick = (e) => {
+    if (!active) {
+      setFavs((prev) => [...prev, record.id]);
+    } else {
+      const arr = [...favs];
+      arr.splice(
+        arr.findIndex((el) => el === record.id),
+        1
+      );
+      setFavs(arr);
+    }
+    setActive((prev) => !prev);
+  };
+
+  return (
+    <td className="p-2 text-sm sm:text-base">
+      {/* <button onClick={handleClick}>{active ? '❤️' : '🤍'}</button> */}
+      <input
+        type="checkbox"
+        checked={active}
+        name={record.id}
+        onChange={handleClick}
+      />
+    </td>
+  );
+};
 
 type Column = { header: string; component: React.ReactNode; colWidth: string };
 
@@ -29,8 +58,8 @@ const columns: Column[] = [
     colWidth: 'w-2',
   },
   {
-    header: '❤️',
-    component: <FavoriteButton />,
+    header: ' ',
+    component: <FavoriteButton record />,
     colWidth: 'w-3',
   },
 ];
@@ -55,6 +84,12 @@ const Sheet = () => {
     () => getSheet(id)
   );
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('clicked submit');
+    // fetch post array of favs
+  };
+
   if (isLoading) return <div>Loading...</div>;
 
   if (isError) {
@@ -69,11 +104,24 @@ const Sheet = () => {
             {sheetName} keyboard shortcuts
           </h1>
         </div>
-        <KeybindList
-          sheetData={data.keybinds}
-          columns={columns}
-          titleField="cheatsheetCategory"
-        />
+        <FavsProvider>
+          <form>
+            <KeybindList
+              sheetData={data.keybinds}
+              columns={columns}
+              titleField="cheatsheetCategory"
+            />
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="w-full max-w-md mt-11 bg-gray-700 text-white px-6 py-3 mb-1 mr-1 text-sm font-bold uppercase transition-all duration-150 ease-linear rounded shadow outline-none hover:shadow-lg focus:outline-none"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </FavsProvider>
       </div>
     );
   } else {
