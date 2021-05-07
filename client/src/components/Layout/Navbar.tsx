d5d33fff4989678a2544dbf1bf795f7ae2f9f64e
aliselmes <a.selmes@gmail.com>
2021-05-06 19:18:31 -0500

    adds favorits link and displays username

--- client/src/components/Layout/Navbar.tsx
+++ client/src/components/Layout/Navbar.tsx
@@ -66,31 +66,50 @@ export default function Navbar() {
                   className="flex items-center px-3 py-2 text-xs text-base font-bold leading-snug text-white border-b-2 border-white border-opacity-0 hover:border-green-300 focus:outline-none transition duration-500 ease-in-out"
                   type="button"
                   >
-                    Sheets
+                    View All
                   </button>
                 </a>
               </li>
-              <li className="nav-item">
-               {isLoggedIn 
+               {isLoggedIn
                ?
-                <button
-                  className="flex items-center px-2 py-2 text-xs text-base font-bold leading-snug text-white border-b-2 border-white border-opacity-0 hover:border-green-300 focus:outline-none transition duration-500 ease-in-out focus:outline-none"
-                  type="button"
-                  onClick={() => logout()}
-                >
-                  Logout
-                </button>
+               <>
+                <li className="nav-item mr-3">
+                  <a href="/" className="hover:no-underline">
+                    <button
+                    className="flex items-center px-3 py-2 text-xs text-base font-bold leading-snug text-white border-b-2 border-white border-opacity-0 hover:border-green-300 focus:outline-none transition duration-500 ease-in-out"
+                    type="button"
+                    >
+                      Favorites
+                    </button>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <button
+                    className="flex items-center px-2 py-2 text-xs text-base font-bold leading-snug text-white border-b-2 border-white border-opacity-0 hover:border-green-300 focus:outline-none transition duration-500 ease-in-out focus:outline-none"
+                    type="button"
+                    onClick={() => logout()}
+                  >
+                    Logout
+                  </button>
+                </li>
+                <li className="nav-item flex items-center md:px-2 py-2 text-xs text-base font-bold leading-snug text-green-300">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                  </svg>
+                  <p className="ml-1">Username</p>
+                </li>
+               </>
               :
-                <button
-                  className="flex items-center px-2 py-2 text-xs text-base font-bold leading-snug text-white border-b-2 border-white border-opacity-0 hover:border-green-300 focus:outline-none transition duration-500 ease-in-out focus:outline-none"
-                  type="button"
-                  onClick={() => setShowModal(true)}
-                >
-                  LOGIN / Sign up
-                </button>
+                <li className="nav-item">
+                  <button
+                    className="flex items-center px-2 py-2 text-xs text-base font-bold leading-snug text-white border-b-2 border-white border-opacity-0 hover:border-green-300 focus:outline-none transition duration-500 ease-in-out focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(true)}
+                  >
+                    LOGIN / Sign up
+                  </button>
+                </li>
               }
-              </li>
-
             </ul>
           </div>
         </div>
