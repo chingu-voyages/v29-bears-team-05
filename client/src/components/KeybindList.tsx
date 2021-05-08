b3f0539ed1497559e82b09bd5a5260efe40257e2
Seth Anderson <sethdanderson17@gmail.com>
2021-05-07 20:20:31 -0700

    Fix category orders changing when adding/deleting

--- client/src/components/KeybindList.tsx
+++ client/src/components/KeybindList.tsx
@@ -1,8 +1,6 @@
 import React, { useMemo } from 'react';
 import _ from 'lodash';
 import Table from '../ui/Table';
-import { useFavs } from '../context/FavContext';
-import { addFavorites } from '../service/queryFns';
 
 type Props = {
   sheetData: any[];
@@ -11,20 +9,15 @@ type Props = {
 };
 
 const KeybindList = ({ sheetData, columns, titleField }: Props) => {
+  // makes sure categories always in original order
   const categories = useMemo(
-    () => Array.from(new Set(sheetData.map((el) => el[titleField].name))),
+    () =>
+      _.uniqBy(sheetData, (el) => el[titleField].name)
+        .sort((a, b) => (a[titleField].index > b[titleField].index ? 1 : -1))
+        .map((el) => el[titleField].name),
+
     [sheetData, titleField]
   );
-
-  // const { favs, setFavs } = useFavs();
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log('clicked submit');
-  //   console.log('favs', favs);
-  //   addFavorites(favs);
-  //   // route to favs sheet ?
-  // };
 
   return (
     <div>
@@ -36,15 +29,6 @@ const KeybindList = ({ sheetData, columns, titleField }: Props) => {
           <Table key={i} title={title} data={tableData} columns={columns} />
         );
       })}
-      {/* <div className="flex justify-center">
-        <button
-          type="submit"
-          onClick={handleSubmit}
-          className="w-full max-w-md mt-11 bg-gray-700 text-white px-6 py-3 mb-1 mr-1 text-sm font-bold uppercase transition-all duration-150 ease-linear rounded shadow outline-none hover:shadow-lg focus:outline-none"
-        >
-          Save
-        </button>
-      </div> */}
     </div>
   );
 };
